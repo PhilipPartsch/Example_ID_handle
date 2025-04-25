d35644793174eb881207725a078d2baa36209ca3
PhilipPartsch <95444300+PhilipPartsch@users.noreply.github.com>
2025-04-25 13:24:33 +0200

    fix internal config of sphinx-needs

--- docs/conf.py
+++ docs/conf.py
@@ -184,3 +184,14 @@ def setup(app):
     for func in metamodel.needs_functions:
         add_dynamic_function(app, func)
 
+# fix shinx-needs
+from sphinx_needs.data import NeedsCoreFields
+patched_options = [
+    ('collapse', 'bool'), ('hide', 'bool'),
+    ('template', 'str'), ('pre_template', 'str'), ('post_template', 'str'),
+    ('type_color', 'str'), ('type_style', 'str'),
+]
+
+for po_name, po_type in patched_options:
+    NeedsCoreFields[po_name]["allow_default"] = po_type
+
